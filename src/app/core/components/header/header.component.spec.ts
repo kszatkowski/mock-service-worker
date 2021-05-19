c37ade1be24ae31b2f4817f5b451463fac02f8de
kszatkowski <kszatkowski@outlook.com>
2021-05-19 18:15:28 +0200

    HeaderComponent - Toggle method tests

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
 
 import { HeaderComponent } from './header.component';
+import { ThemeService } from '../../services/theme.service';
 
 describe('HeaderComponent', () => {
   let spectator: Spectator<HeaderComponent>;
@@ -13,5 +14,24 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(spectator.component).toBeTruthy();
+  });
+
+  describe('toggle', () => {
+    it('should call toggleTheme method from themeService', () => {
+      const themeService = spectator.inject(ThemeService);
+      const themeServiceSpy = spyOn(themeService, 'toggleTheme');
+      spectator.component.toggle();
+      expect(themeServiceSpy).toHaveBeenCalled();
+    });
+
+    it('should change icon to dark_mode', (done) => {
+      spectator.component.toggle();
+      spectator.detectChanges();
+      spectator.component.isDarkTheme$.subscribe((value) => {
+        expect(value).toBeTruthy();
+        expect(spectator.query('mat-icon')?.textContent).toBe('dark_mode');
+        done();
+      });
+    });
   });
 });
